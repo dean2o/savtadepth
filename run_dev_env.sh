9293d40b86da63af827a938fb83f5cd55c0f3e8d
Dean <dean@dagshub.com>
2020-10-16 11:33:48 +0000

    Added escaping slash to run_dev_env.sh so that it works in windows as well

--- run_dev_env.sh
+++ run_dev_env.sh
@@ -1,6 +1,6 @@
 docker run -d \
     -p 8080:8080 \
-    --name "dags-ml-workspace" -v "${PWD}:/workspace" \
+    --name "dags-ml-workspace" -v "/${PWD}:/workspace" \
     --env AUTHENTICATE_VIA_JUPYTER="dagshub_savta" \
     --shm-size 2G \
     --restart always \
